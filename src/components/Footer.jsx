d8bcd856557d783508cfd9af46ba6d26a58bdffb
kentaurse <kentaurse0212@gmail.com>
2024-10-01 08:11:23 +0800

    fix: footer name

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,7 +4,7 @@ const Footer = ({ ...props }) => {
   return (
     <footer className={props.className}>
       <div className='footer flex justify-center items-center w-full h-28 m-auto'>
-        <p>&copy; 2024 PLAZA HOMES, LTD.</p>
+        <p>&copy; 2024 プッシュ通知システム.</p>
       </div>
     </footer>
   );
